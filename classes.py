b9bcf3aa57b0ece39b7410a2cf173d35899d3769
luanpeneluca <110501160+luanpeneluca@users.noreply.github.com>
2025-04-26 15:05:37 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -3,3 +3,36 @@ class Cliente:
         self.nome = nome
         self.email = email
 
+
+#classe produtos
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"Produto: {self.name}\nPreço: R${self.price:.2f}"
+
+
+#classe cardapio
+class Menu:
+    def __init__(self):
+        self.products = []
+
+    #funcao addiconar produto
+    def add_product(self, product):
+        if product not in self.products:
+            self.products.append(product)
+
+    #funcao remover produto
+    def remove_product(self, product):
+        for p in self.products:
+            if p == product:
+                self.products.remove(product)
+                return    
+        return
+    
+    #funcao imprimir cardapio
+    def show_menu(self):
+        for p in self.products:
+            print(p)
