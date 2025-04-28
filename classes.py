037d1dcf6be3c733a2e1faa2bf7d8cde6d36b56c
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2025-04-28 09:28:41 -0300

    creating classes

--- classes.py
+++ classes.py
@@ -1,31 +1,48 @@
 #Cliente
 class Customer:
-    def __init__(self, name, email):
+    def __init__(self, name, phone_number, address):
         self.name = name
-        self.email = email
+        self.phone_number = phone_number
+        self.address = address
+
+#Produto
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"Produto: {self.name}\nPreço: R${self.price:.2f}"
 
 #Pedido
 class Order:
-    def __init__(self, customer, price, order_number):
+    def __init__(self, customer: Customer, value, order_number):
         self.name = customer.name
-        self.email = customer.email
-        self.price = price
+        self.phone_number = customer.phone_number
+        self.value = value
         self.order_number = order_number
 
     #caso dê o print(pedido), retorna as info
     def __str__(self):
         return f"Cliente: {self.name}\nEmail: {self.email}\nValor: {self.price:.2f}\nNúmero do Pedido: {self.order_number}"
 
-######classes que não serão usadas agora#######
+#Método de pagamento
+class Method:
+    cash = "Dinheiro"
+    debit_card = "Cartão de débido"
+    credit_card = "Cartão de crédito"
+    pix = "Pix"
 
-#Produtos
-#class Product:
-#    def __init__(self, name, price):
-#        self.name = name
-#        self.price = price
-#
-#    def __str__(self):
-#        return f"Produto: {self.name}\nPreço: R${self.price:.2f}"
+#Pagamento
+class Payment:
+    def __init__(self, order: Order, method: Method):
+        self.order = order
+        self.method = method
+
+
+
+
+######classes que não serão usadas agora#######
 
 #Cardapio
 #class Menu:
