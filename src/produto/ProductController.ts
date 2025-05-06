897450d00933e62b59c638e386cc6a9e8c68a6a9
Arnaldo Liro <dinholiro@gmail.com>
2025-05-06 12:13:16 -0300

    fix: logic of creating a product

--- src/produto/ProductController.ts
+++ src/produto/ProductController.ts
@@ -6,12 +6,22 @@ export async function registrarProduto(req: Request, res: Response) {
         const { data } = req.body
 
         if(!data){
-            return res.status(500).json({ erro: "Erro na requisição" })
+            return res.status(500).json({ error: "Erro na requisição" })
         }
 
         const produto = await criarProduto(data);
-        
-    } catch(error) {
 
+        if(!produto) {
+            return res.status(400).json({ error: "Falaha ao criar produto"})
+        }
+
+        console.log(`Produto criado com sucesso! ${produto}`)
+        return res.status(201).json({
+            message: "Produto criado com sucesso!",
+            produto
+        })
+    } catch(error) {
+        console.log(error)
+        return res.status(500).json({error: "Internal Server Error"})
     }
 }
