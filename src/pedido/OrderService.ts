d0f4697e3f9800644f08a617fcec9cc780ca5d75
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-05-18 17:42:56 -0300

    fix: corrigindo OrderController para pagamento com cartao

--- src/pedido/OrderService.ts
+++ src/pedido/OrderService.ts
@@ -24,7 +24,7 @@ export async function criarPedido(
             throw new Error("Dados do cartão são obrigatórios para esse método.");
         }
 
-        pagamento = `autorizado-${randomInt(1000, 9999)}`;
+        pagamento = `autorizado-${randomInt(1000, 9999)}`; // simula aprovaçao
     }
     else {
         throw new Error("Ainda não existe outro método de pagamento ou método não suportado.")
