d97ba2f3008166b8dc9ed11d8057e72862f2f83a
Arnaldo Liro <dinholiro@gmail.com>
2025-05-19 13:47:42 -0300

    fix: creating CRUD for product route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { deleteUser, getAllClients, getOneClient, registerClient } from "./cliente/ClientController";
 import { registrarPedido, verificarPedido, confirmarPagamento } from "./pedido/OrderController";
+import { deletarProduto, getAllProducts, getOneProduct, registrarProduto } from "./produto/ProductController";
 
 
 const router = Router()
@@ -10,7 +11,7 @@ router.get('/', (req, res) => {
 })
 
 // Rotas para Clientes
-router.post('/register', (req, res, next) => {
+router.post('/cliente/register', (req, res, next) => {
     registerClient(req, res).catch(next);
 });
 router.get('/clientes', (req, res, next) => {
@@ -22,12 +23,20 @@ router.get('/cliente', (req, res, next) => {
 router.delete('/cliente', (req, res, next) => {
   deleteUser(req,res).catch(next);
 })
-router.put('/cliente', (req, res, next) => {
-  // getOneClient(req,res).catch(next);
-})
 
 // Rota para os Produtos
-
+router.post('/produtos/register', (req, res, next) => {
+    registrarProduto(req, res).catch(next);
+});
+router.get('/produtos', (req, res, next) => {
+  getAllProducts(req,res).catch(next);
+})
+router.get('/produto', (req, res, next) => {
+  getOneProduct(req,res).catch(next);
+})
+router.delete('/produto', (req, res, next) => {
+  deletarProduto(req,res).catch(next);
+})
 
 // Rotas para os Pedidos
 router.post('/pedidos', (req, res, next) => {
