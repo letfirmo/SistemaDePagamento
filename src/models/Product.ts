5addd3678810b7e9b27eca873a8f6f7d956c7428
luanpeneluca <110501160+luanpeneluca@users.noreply.github.com>
2025-07-14 18:28:36 -0300

    feature classe Product

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,1 +1,17 @@
 // Entidade produto
+
+export class Product {
+    constructor(
+        public readonly id: number,
+        public name:string,
+        public price: number
+    ) {
+        if (price < 0) {
+            throw new Error("O preço do produto não pode ser negativo.")
+        }
+
+        if (name.trim() === "") {
+            throw new Error("O nome do produto não pode ser vazio.")
+        }
+    }   
+}
