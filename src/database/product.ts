fc92958767eeab2ef7acbb811edd1c8fc884e474
Arnaldo Liro <dinholiro@gmail.com>
2025-07-14 23:10:08 -0300

    feat: adicionando dados do product

--- src/database/product.ts
+++ src/database/product.ts
@@ -1,1 +1,9 @@
+import { Product } from "../models/Product";
 
+export const products = [
+  new Product(1, "Notebook", 3500),
+  new Product(2, "Teclado Mecânico", 450),
+  new Product(3, "Mouse Gamer", 230),
+  new Product(4, "Monitor 24\"", 899.99),
+  new Product(5, "Headset", 329.5)
+];
