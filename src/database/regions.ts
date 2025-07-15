d1c7a3d1866a1c2e1d6d7d74911f86219027eb59
Arnaldo Liro <dinholiro@gmail.com>
2025-07-14 23:11:32 -0300

    fix: regions database

--- src/database/regions.ts
+++ src/database/regions.ts
@@ -1,7 +1,14 @@
-import { Region } from "../models/Region"
+import { Region } from "../models/Region";
 
-export const region = [
-    new Region ('1','Brotas', 12.8),
-    new Region ('2','Pau da Lima', 20),
-    new Region ('3','Ondina', 17)
-]
+export const regions = [
+  new Region('1', 'Brotas', 12.8),
+  new Region('2', 'Pau da Lima', 20),
+  new Region('3', 'Ondina', 17),
+  new Region('4', 'Centro', 10),
+  new Region('5', 'Itapuã', 18.5),
+  new Region('6', 'Liberdade', 13.2),
+  new Region('7', 'Barra', 15),
+  new Region('8', 'Bonfim', 14.5),
+  new Region('9', 'Cabula', 16.7),
+  new Region('10', 'São Caetano', 19)
+];
