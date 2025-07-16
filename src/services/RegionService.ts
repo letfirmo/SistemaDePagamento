e9a68c0f9a2a646f45c7636bd2b3c129dc8b85fe
AlissonRibeiro <alissonribeiro@ufba.br>
2025-07-16 14:06:18 -0300

    creat class RegionService.ts

---
+++ src/services/RegionService.ts
@@ -0,0 +1,23 @@
+import { Region } from './models/Region';
+import { regions } from './base de dados/regions';
+
+export class RegionService {
+  private regions = regions;
+
+  add(region: Region): void {
+    if (this.regions.some(r => r.id === region.id)) {
+      throw new Error(`Região com ID ${region.id} já existe.`);
+    }
+    this.regions.push(region);
+  }
+
+  getAll(): Region[] {
+    return [...this.regions];
+  }
+
+  findById(id: string): Region | undefined {
+    return this.regions.find(r => r.id === id);
+  }
+}
+
+export const regionService = new RegionService();
