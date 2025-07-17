54ff21041047ff94866b63c0b5c36cd943d1b12c
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-07-17 14:52:21 -0300

    fix: adicionando ReportService ao app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { products } from "./database/product"
 import { Pix } from "./models/Pix"
 import { Card, CardType } from "./models/Card"
 import { regionService } from "./services/RegionService"
+import { ReportService } from "./services/ReportService"
 
 // Criando regiões
 const simoesfilho = regionService.add({ id:'23', name:'Simões Filho', delivery:34.67 });
@@ -16,19 +17,30 @@ const cliente1 = customerService.add({ id:"10", name:"Ana", region: simoesfilho}
 const cliente2 = customerService.add({ id:"34", name:"Carlos", region: brotas});
 
 // Criando pedidos
-const order1 = orderService.createOrder("1", cliente1, [products[0]], new Pix('999999999'));
-const order2 = orderService.createOrder("2", cliente2, [products[2]], new Card('234 3445 2344', CardType.Debit));
+orderService.createOrder("1", cliente1, [products[0]], new Pix('999999999'));
+orderService.createOrder("2", cliente2, [products[2]], new Card('234 3445 2344', CardType.Debit));
+orderService.createOrder("3", customers[0], [products[3], products[0]], new Card('73736 3664 44', CardType.Credit));
 
 // Pagando pedidos
 const pagamento1 = orderService.payOrder("1");
 const pagamento2 = orderService.payOrder("2");
+const pagamento3 = orderService.payOrder('3');
+
+//Relatórios
+const report = ReportService.generateDailyReport(orderService.getAll())
+
 
 // Mostrando resultados
-console.log("PEDIDOS:");
+console.log('--------------------------------PEDIDOS---------------------------------')
+console.log('')
 orderService.getAll().forEach(order => {
-  console.log(`Pedido ${order.id} | Cliente: ${order.customer.name} | Total: R$${order.amount.toFixed(2)} | Pago: ${order.paid}`);
+    console.log(`Pedido ${order.id} | Cliente: ${order.customer.name} | Itens: ${order.items.map(i => i.name)} | Total: R$${order.amount.toFixed(2)} | Taxa de entrega: ${order.customer.region.delivery.toFixed(2)} | Pago: ${order.paid}`);
 });
 
-console.log("\nPAGAMENTOS:");
+console.log("\nPAGAMENTOS:\n");
 console.log(pagamento1);
 console.log(pagamento2);
+console.log(pagamento3)
+
+console.log('\n--------------------------------RELATÓRIOS---------------------------------\n')
+console.log(report);
--- src/database/product.ts
+++ src/database/product.ts
@@ -1,9 +1,9 @@
 import { Product } from "../models/Product";
 
 export const products = [
-  new Product(1, "Sashimis", 50),
-  new Product(2, "Barca de sushis", 189),
-  new Product(3, "Salada de Polvo", 80),
-  new Product(4, "Temaki de Salmão Grelhado", 46.99),
-  new Product(5, "Temaki de Camarão", 40.80)
+  new Product(1, " Sashimis ", 50),
+  new Product(2, " Barca de sushis ", 189),
+  new Product(3, " Salada de Polvo ", 80),
+  new Product(4, " Temaki de Salmão Grelhado ", 46.99),
+  new Product(5, " Temaki de Camarão ", 40.80)
 ];
